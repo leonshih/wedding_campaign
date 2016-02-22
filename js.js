e582b714bc815aed91eb128af38d6dc1cf3019ef
leonshih <leonshih725@gmail.com>
2016-02-22 12:41:44 +0800

    02221241

---
+++ js.js
@@ -0,0 +1,4 @@
+$(function(){
+	window.open('https://api.instagram.com/oauth/authorize/?client_id=04267db24e594d788cb8b54c4c3cc388&redirect_uri=http://leonshih.github.io/wedding_campaign/&response_type=code');
+	
+});
