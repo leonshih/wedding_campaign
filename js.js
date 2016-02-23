ac29af5c119831337c5c0ea4695176ad814007cb
leonshih <leonshih725@gmail.com>
2016-02-23 09:57:47 +0800

    .

--- js.js
+++ js.js
@@ -1,4 +1,16 @@
 $(function(){
-	window.open('https://api.instagram.com/oauth/authorize/?client_id=04267db24e594d788cb8b54c4c3cc388&redirect_uri=http://leonshih.github.io/wedding_campaign/&response_type=code');
-	
+	check_state();	
 });
+
+function check_state(){
+	var params = {},
+		queryString = location.search.replace('?',''),
+		regex = /([^&=]+)=([^&]*)/g, 
+		m;
+	while(m = regex.exec(queryString)){
+		params[decodeURICompent(m[1])] = decodeURICompent(m[2]);
+	}
+	if(params['code'] == null)
+		window.location.href = 'https://api.instagram.com/oauth/authorize/?client_id=04267db24e594d788cb8b54c4c3cc388&redirect_uri=http://leonshih.github.io/wedding_campaign/&response_type=code';
+	
+}
