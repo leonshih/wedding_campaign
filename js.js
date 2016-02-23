359dc270c2ea6197fa1f9bd6b71484eda415aa55
leonshih <leonshih725@gmail.com>
2016-02-23 10:00:57 +0800

    .

--- js.js
+++ js.js
@@ -8,7 +8,7 @@ function check_state(){
 		regex = /([^&=]+)=([^&]*)/g, 
 		m;
 	while(m = regex.exec(queryString)){
-		params[decodeURICompent(m[1])] = decodeURICompent(m[2]);
+		params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
 	}
 	if(params['code'] == null)
 		window.location.href = 'https://api.instagram.com/oauth/authorize/?client_id=04267db24e594d788cb8b54c4c3cc388&redirect_uri=http://leonshih.github.io/wedding_campaign/&response_type=code';
