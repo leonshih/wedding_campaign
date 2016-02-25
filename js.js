72d061f1aff9d3430edf958d783643b9390e5e11
leonshih <leonshih725@gmail.com>
2016-02-25 16:38:36 +0800

    .

--- js.js
+++ js.js
@@ -1,28 +1,32 @@
 //mine
 //var access_token = "37246735.04267db.6592b801aa5f4ebf91af1e9a0dfe3f3b",
 //other
-//var access_token = "2274610174.1fb234f.5f6eb6bc393c4888a2e1a045490784d9",
-var access_token = "16384709.6ac06b4.49b97800d7fd4ac799a2c889f50f2587",
+var access_token = "2274610174.1fb234f.5f6eb6bc393c4888a2e1a045490784d9",
+//var access_token = "16384709.6ac06b4.49b97800d7fd4ac799a2c889f50f2587",
+client_id = '04267db24e594d788cb8b54c4c3cc388', //mine
+//client_id = '81e3d3f35c8a4438964001decaa5a31f',
 
     access_parameters = {
         access_token: access_token
     };
 
 $(function() {
-    //grabImages('puppy', 35, access_parameters);
+    callAPI();
+});
 
-    // Get an instance of a fetcher
-    var fetcher = new Instafetch('81e3d3f35c8a4438964001decaa5a31f');
+function callAPI(){
+	// Get an instance of a fetcher
+    var fetcher = new Instafetch(client_id);
 
     // Fetch results
     fetcher.fetch({
         user: '37246735', // This is the ID for 9gag, you can get the ID from username using http://jelled.com/instagram/lookup-user-id
-        //tag: 'puppy',
+        //tag: 'gibson',
         limit: 50,
         callback: onDataLoaded, // You can customize your own functionality by building from the logResults method
         params: 'yiyi & leon'
     });
-});
+}
 
 
 function grabImages(tag, count, access_parameters) {
@@ -34,6 +38,7 @@ var bgPhotos;
 var bgCount = 0;
 var bgMaxCount;
 var photoArr;
+var bgTimer;
 function onDataLoaded(instagram_data) {
     console.log(instagram_data);
     var target = $("#target");
@@ -52,28 +57,29 @@ function onDataLoaded(instagram_data) {
 	photoArr = photos;
 	bgPhotos = $('.bg-photo');
 	bgMaxCount = bgPhotos.length;
-	var bgTimer = setInterval(bgInterval, 3500);
+	bgTimer = setInterval(bgInterval, 3500);
 }
 
+var fadeTime = 800;
 function bgInterval(){
-	var stPhoto = $('.standardPhoto');
-	
 	if(bgCount == bgMaxCount)
 	{
-		$(bgPhotos[bgCount]).fadeTo(500, 0);
-		bgCount = 0;
-		$(bgPhotos[bgCount]).fadeTo(500, 1);
+		clearInterval(bgTimer);
+		callAPI();
 	}
 	else
 	{
-		$(bgPhotos[bgCount-1]).fadeTo(500, 0.2);
-		$(bgPhotos[bgCount]).fadeTo(500, 1);
+		$(bgPhotos[bgCount-1]).fadeTo(fadeTime, 0.2);
+		$(bgPhotos[bgCount]).fadeTo(fadeTime, 1);
 	}
+	$('.standardPhoto')[0].src = photoArr[bgCount].images.standard_resolution.url;
+	$('.user_pic')[0].src = photoArr[bgCount].user.profile_picture;
+	$('.username').html(photoArr[bgCount].user.username);
 	
-	stPhoto[0].src = photoArr[bgCount].images.standard_resolution.url;
-	stPhoto.fadeTo(500, 0.2, function(){
-		stPhoto.fadeTo(500, 1);
+	$('.main_area').fadeTo(fadeTime, 0, function(){
+		$('.main_area').fadeTo(fadeTime, 1);
 	});
+	
 	bgCount++;
 	
 }
